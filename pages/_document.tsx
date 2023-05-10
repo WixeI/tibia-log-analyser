4c6965a24e980928b6c319288f1716cc97e1d721
Paulo Martins <pauloportelamartins@gmail.com>
2023-05-10 17:05:02 -0300

    Added styles & libs

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,13 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body className="bg-neutral-700 text-neutral-50">
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+const colors = require("tailwindcss/colors");
+
+module.exports = {
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}"
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: colors.amber,
+        secondary: colors.red,
+        neutral: colors.stone
+      }
+    }
+  },
+  plugins: ["prettier-plugin-tailwindcss"]
+};
